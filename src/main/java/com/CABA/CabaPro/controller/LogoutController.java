be3331517c1720c4fdc3d0063052092bee206251
arangoj20 <juanmanuelmontoya05@gmail.com>
2025-09-09 21:03:57 -0500

    se arreglan algunas rutas dalñadas y se crea LogoutController

---
+++ src/main/java/com/CABA/CabaPro/controller/LogoutController.java
@@ -0,0 +1,19 @@
+package com.CABA.CabaPro.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
+}
